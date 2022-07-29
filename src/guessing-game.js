54fd2ec36d18cdb8fc9e15fb68883124ad47c659
Areliya13 <sharipovatamara@gmail.com>
2022-07-30 02:16:51 +0300

    feat: implement code to src

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2)
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max) / 2)
     }
 }
 
